fe903e388762c745517e3d6ec35aa29f9c6bcd78
Elvin132 <elvinarisdo@gmail.com>
2025-04-07 23:40:59 +0700

    Add files via upload
    Learn

---
+++ Coba Coding/data.js
@@ -0,0 +1,29 @@
+const nama = "Elvin Arisdo";
+let usia = 10;
+
+let biodata = document.getElementById('biodata'); 
+
+function generateBiodata() {
+    let generasi;
+
+    if(usia > 10 && usia < 18){
+        generasi = "Generasi Remaja";
+    }else if( usia >= 18 && usia < 30) {
+        generasi = "Generasi Dewasa";
+    }else if(usia >= 30) {
+        generasi = "Generasi Tua";
+    }else if(usia >= 2 && usia < 10){
+        generasi = "Generasi Anak2";
+    }else{
+        generasi = "Generasi Bayi";;
+    }
+
+    return biodata.innerHTML = generasi;
+
+
+}
+
+console.log(nama);
+console.log(usia);
+
+generateBiodata();
